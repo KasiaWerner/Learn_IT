98ae1e0b67eb67c64426e18634c9d5221a2a749b
KasiaWerner <kwerner@centrumcyfrowe.pl>
2015-03-21 20:39:45 +0100

    updated classes

--- src/kayak_map/Obstacle.java
+++ src/kayak_map/Obstacle.java
@@ -7,26 +7,31 @@ public class Obstacle {
 
     private String obstacleName;
     private Point2D.Double obstaclePosition;
-  
+ 
     public Obstacle (String name, double positionX, double positionY){
-      
+     
         obstacleName = name;
         obstaclePosition = new Point2D.Double(positionX, positionY);
+     
+    }
+ 
+    public static void main(String[] args) {
+     
+        Obstacle rock = new Obstacle("Rock", 25, 45);
+       
+        //Then, in the main method:
+           
+        rock.changeName("Stone");
+
+        System.out.println(rock.getName()); 
       
     }
   
-    public static void main(String[] args) {
-      
-        Obstacle rock = new Obstacle("Rock", 25, 45);
-       
-        rock.obstacleName.replaceAll("Rock", "Stone"); //testing replaceObstacleName method
-        System.out.println(rock.obstacleName); //printed result: Rock
-    }
-   
-    public String replaceObstacleName(String regex, String replacement){
-        obstacleName.replaceAll(regex, replacement);
-        return replacement;
-    }
-   
-
+    public void changeName(String newName) {
+        this.obstacleName = newName;
 }
+    
+    public String getName(){
+    	return this.obstacleName;
+    }
+    }
